2ac14ff580507c4952e0c1be583fffd0351291f1
PRoy <sam469roger@gmail.com>
2024-05-08 09:44:02 +0530

    setup database and server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import http from 'http';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+const port = process.env.PORT || 4000;
+const DB = process.env.MONGODB_URL;
+const server = http.createServer(app);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log('Database successfully connected...');
+    server.listen(port, () => {
+      console.log(`Server running on port: ${port}...`);
+    });
+  } catch (err) {
+    console.log(err.name, err.message);
+  }
+};
+
+startServer();
