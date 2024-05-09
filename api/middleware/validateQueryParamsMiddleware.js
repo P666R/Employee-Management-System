d6482cfc4c32cd91f5a7f1a3dbfb8e00ccf03aa4
PRoy <sam469roger@gmail.com>
2024-05-09 20:51:30 +0530

    refactored validateQueryParams to allow legit params only

--- api/middleware/validateQueryParamsMiddleware.js
+++ api/middleware/validateQueryParamsMiddleware.js
@@ -1,11 +1,24 @@
 import { validationResult } from 'express-validator';
 
+const allowedParams = ['search', 'page', 'limit', 'sortBy'];
+
 const validateQueryParams = (req, res) => {
+  const unexpectedParams = Object.keys(req.query).filter(
+    (param) => !allowedParams.includes(param),
+  );
+
+  if (unexpectedParams.length > 0) {
+    res.status(400);
+    throw new Error(
+      `Unexpected query parameters: ${unexpectedParams.join(', ')}`,
+    );
+  }
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400);
     throw new Error(
-      `Validation failed - ${errors
+      `Validation failed: ${errors
         .array()
         .map((e) => e.msg)
         .join(', ')}`,
