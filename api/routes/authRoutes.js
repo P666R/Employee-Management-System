45b1513bc60bb17659109aceee550874ec4c2bb2
PRoy <sam469roger@gmail.com>
2024-05-09 11:23:32 +0530

    implemented logoutController

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -3,6 +3,7 @@ import registerUser from '../controllers/auth/registerController.js';
 import loginUser from '../controllers/auth/loginController.js';
 import { loginLimiter } from '../middleware/apiLimiter.js';
 import newAccessToken from '../controllers/auth/refreshTokenController.js';
+import logoutUser from '../controllers/auth/logoutController.js';
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post('/register', registerUser);
 router.post('/login', loginLimiter, loginUser);
 
 router.get('/new_access_token', newAccessToken);
+
+router.get('/logout', logoutUser);
 
 export default router;
