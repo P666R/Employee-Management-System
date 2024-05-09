69d7507791c091588f7236e5906f1bd172e1d5dc
PRoy <sam469roger@gmail.com>
2024-05-09 10:49:38 +0530

    implemented refreshTokenController

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -2,11 +2,14 @@ import express from 'express';
 import registerUser from '../controllers/auth/registerController.js';
 import loginUser from '../controllers/auth/loginController.js';
 import { loginLimiter } from '../middleware/apiLimiter.js';
+import newAccessToken from '../controllers/auth/refreshTokenController.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 
 router.post('/login', loginLimiter, loginUser);
+
+router.get('/new_access_token', newAccessToken);
 
 export default router;
